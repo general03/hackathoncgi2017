2706893101b7346f070b914852cb4abadb6462fd
cgi <cgi@FR-L2189244.groupinfra.com>
2017-06-22 19:12:28 +0200

    Update urgency page

--- src/main/webapp/app/urgency/urgency.controller.js
+++ src/main/webapp/app/urgency/urgency.controller.js
@@ -5,15 +5,17 @@
         .module('theaApp')
         .controller('UrgencyController', UrgencyController);
 
-    UrgencyController.$inject = ['$scope', '$state'];
+    UrgencyController.$inject = ['$scope', '$state', '$location', 'toaster'];
 
-    function UrgencyController ($scope, $state) {
+    function UrgencyController ($scope, $state, $location, toaster) {
         var vm = this;
 
-        vm.register = register;
+        $scope.goPop = function(){
+	        toaster.pop('warning', "1er geste à accomplir en cas de traumatisme", "<br> • Ne pas frotter l'oeil <br> • Placer une compresse sur la paupierre superposé");
+	    };
 
-        function register () {
-            $state.go('register');
-        }
+        $scope.goWatson = function() {
+            $location.url('/watson');
+        };
     }
 })();
