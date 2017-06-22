c1b4e954d775b0b2926099fe535fb17beaab97c6
guelerauda <antoine.gueleraud@gmail.com>
2017-06-22 22:31:27 +0200

    New version of watson screen

--- src/main/webapp/app/watson/watson.controller.js
+++ src/main/webapp/app/watson/watson.controller.js
@@ -10,28 +10,34 @@
     function WatsonController ($scope, $state, $http) {
         var vm = this;
 
-        vm.context = null;
-        vm.question = null;
-        vm.answer = null;
+        vm.conversation = {
+            context: {},
+            question: "",
+            responses: []
+        };
 
-        vm.register = register;
-        function register () {
-            $state.go('register');
-        }
-
-        vm.initConversation = initConversation;
-        function initConversation() {
-            context = null;
+        // Cette méthode permet de lancer la conversation avec watson et de recevoir la question initialle.
+        (function initConversation() {
             $http.get('api/watson-init').then(function(response) {
-                vm.context = response.data;
+                readResponse(response.data);
             });
+        })();
+
+        function readResponse (response) {
+            var responseJson = JSON.parse(response.text[0].replace(/'/g, '"'));
+            console.log(responseJson);
+            vm.conversation.context = response.context;
+            vm.conversation.question = responseJson.question;
+            vm.conversation.responses = responseJson.responses;
+            console.log(vm.conversation);
         }
+
+
 
         vm.interact = interact;
         function interact() {
-            return $http.post('api/watson-conversation',{ 'context' : vm.context, 'message' : vm.message }).then(function(response) {
-                vm.question = response.data.text;
-                vm.context = response.data;
+            return $http.post('api/watson-conversation',{ 'context' : vm.conversation.context, 'message' : "Choix 1"}).then(function(response) {
+
             });
         }
    }
