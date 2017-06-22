896b7dc2956b82ca690b22c4cc542203a9f6a56c
cgi <cgi@FR-L2189244.groupinfra.com>
2017-06-22 21:44:53 +0200

    Change urgency page

--- src/main/webapp/app/urgency/urgency.controller.js
+++ src/main/webapp/app/urgency/urgency.controller.js
@@ -12,10 +12,15 @@
 
         $scope.goPop = function(){
 	        toaster.pop('warning', "1er geste à accomplir en cas de traumatisme", "<br> • Ne pas frotter l'oeil <br> • Placer une compresse sur la paupierre superposé");
+	        document.getElementById('overlay').classList.remove("hide");
 	    };
 
         $scope.goWatson = function() {
             $location.url('/watson');
+        };
+
+        $scope.goHome = function() {
+            $location.url('/');
         };
     }
 })();
