f6a1ad54ce29f085d6e501c690db5ce7ffe4284c
general03 <drigaudie@yahoo.fr>
2017-06-23 09:15:27 +0200

    Update design

--- src/main/webapp/app/urgency/urgency.controller.js
+++ src/main/webapp/app/urgency/urgency.controller.js
@@ -11,9 +11,15 @@
         var vm = this;
 
         $scope.goPop = function(){
-	        toaster.pop('warning', "1er geste à accomplir en cas de traumatisme", "<br> • Ne pas frotter l'oeil <br> • Placer une compresse sur la paupierre superposé \
-	        <br>... \
-	        <br><br><br> Redirection sur la page d'acceuil dans 10 secondes ");
+	        toaster.pop('warning', "1er geste à accomplir en cas de traumatisme", "<h2>Plaie ou contusion par impact</h2> \
+	        • Ne rien mettre dans l'oeil <br> \
+	        • Fermer l'oeil \
+	        • Placer une compresse pliée sur la paupière suprérieure \
+	        • Appliquer un pansement occulaire maintenu par une bande \
+	        <br> \
+	        <h2>Brûlures</h2> \
+	        • Laver l'oeil abondamment sous un filet d'eau froide pendant au moins 10 min en faisant couler le liquide sur le nez afin de laver l'oeil de l'intérieur vers l'exterieur '\
+	        <br><br><br> Redirection sur la page d'accueil dans 10 secondes ");
 	        document.getElementById('overlay').classList.remove("hide");
 
 	        $timeout(function() {
