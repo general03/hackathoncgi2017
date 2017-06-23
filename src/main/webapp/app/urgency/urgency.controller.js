c1565955a6039452ab78fde9e9e6fbb59d142643
cgi <cgi@FR-L2189244.groupinfra.com>
2017-06-23 08:03:07 +0200

    Change urgency page

--- src/main/webapp/app/urgency/urgency.controller.js
+++ src/main/webapp/app/urgency/urgency.controller.js
@@ -5,22 +5,25 @@
         .module('theaApp')
         .controller('UrgencyController', UrgencyController);
 
-    UrgencyController.$inject = ['$scope', '$state', '$location', 'toaster'];
+    UrgencyController.$inject = ['$scope', '$state', '$location', 'toaster', '$timeout'];
 
-    function UrgencyController ($scope, $state, $location, toaster) {
+    function UrgencyController ($scope, $state, $location, toaster, $timeout) {
         var vm = this;
 
         $scope.goPop = function(){
-	        toaster.pop('warning', "1er geste à accomplir en cas de traumatisme", "<br> • Ne pas frotter l'oeil <br> • Placer une compresse sur la paupierre superposé");
+	        toaster.pop('warning', "1er geste à accomplir en cas de traumatisme", "<br> • Ne pas frotter l'oeil <br> • Placer une compresse sur la paupierre superposé \
+	        <br>... \
+	        <br><br><br> Redirection sur la page d'acceuil dans 10 secondes ");
 	        document.getElementById('overlay').classList.remove("hide");
+
+	        $timeout(function() {
+               $location.url('/');
+              }, 10000);
+
 	    };
 
         $scope.goWatson = function() {
             $location.url('/watson');
-        };
-
-        $scope.goHome = function() {
-            $location.url('/');
         };
     }
 })();
