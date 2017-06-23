236db0ba65ef3966177a812ed2a2291566fb1ad4
guelerauda <antoine.gueleraud@gmail.com>
2017-06-23 11:55:54 +0200

    export pdf

--- src/main/java/com/cgi/hackathon/web/rest/PdfExporter.java
+++ src/main/java/com/cgi/hackathon/web/rest/PdfExporter.java
@@ -1,6 +1,6 @@
 package com.cgi.hackathon.web.rest;
 
-import com.cgi.hackathon.domain.Pathogene;
+import com.cgi.hackathon.domain.*;
 import com.cgi.hackathon.service.MailService;
 import com.google.common.collect.Lists;
 import com.itextpdf.io.image.ImageDataFactory;
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -113,6 +114,30 @@ public class PdfExporter {
         document.close();
 
         return pdf.toByteArray();
+    }
+
+    @RequestMapping(value = "/{namePatho}", method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> generatePdf(@PathVariable("namePatho") String namePatho) throws IOException {
+        Pathogene pathogene;
+        if("ConjonctiviteBacterienne".equalsIgnoreCase(namePatho)) {
+            pathogene = new ConjonctiviteBacterienne();
+        } else if ("ConjonctiviteVirale".equalsIgnoreCase(namePatho)) {
+            pathogene = new ConjonctiviteVirale();
+        } else if ("ConjonctiviteAllergique".equalsIgnoreCase(namePatho)) {
+            pathogene = new ConjonctiviteAllergique();
+        } else if ("FragiliteCapillaireConjonctivale".equalsIgnoreCase(namePatho)) {
+            pathogene = new FragiliteCapillaireConjonctivale();
+        } else if ("SecheresseOculaire".equalsIgnoreCase(namePatho)) {
+            pathogene = new SecheresseOculaire();
+        } else if ("IrritationConjonctivaleChroniqueNonInfectieuse".equalsIgnoreCase(namePatho)) {
+            pathogene = new IrritationConjonctivaleChroniqueNonInfectieuse();
+        } else {
+            pathogene = new Pathogene();
+        }
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.APPLICATION_PDF)
+            .body(new InputStreamResource(new ByteArrayInputStream(createPdf(pathogene))));
     }
 
     @RequestMapping(value = "/example", method = RequestMethod.GET)
